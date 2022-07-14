43afba21912151c1789fc8bf0130eb6788c23b2d
jamong <99.1.Some@gmail.com>
2022-07-14 21:30:55 +0900

    base

---
+++ app/src/main/java/com/example/trpg_try/signup_page.kt
@@ -0,0 +1,11 @@
+package com.example.trpg_try
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class signup_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_page)
+    }
+}
