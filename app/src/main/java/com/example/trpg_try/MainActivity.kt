ab0dfed7988a185173330f314ffc0923ff27c704
jamong <99.1.Some@gmail.com>
2022-07-21 19:50:17 +0900

    회원가입 화면

--- app/src/main/java/com/example/trpg_try/MainActivity.kt
+++ app/src/main/java/com/example/trpg_try/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //테스트
+        
 
         // 이제 할 일 : 버튼 누르면 다음 화면 넘어가게
 
